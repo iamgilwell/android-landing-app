7427ae40934c977e524a63becc775387c11e3dae
Gilwell M <gilwellm@gmail.com>
2024-11-10 17:12:10 +0300

    Initialize project

---
+++ app/src/main/java/com/rivabu/ala/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,17 @@
+package com.rivabu.ala.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel :
+    ViewModel() {
+
+    private val _text =
+        MutableLiveData<String>().apply {
+            value =
+                "This is gallery Fragment"
+        }
+    val text: LiveData<String> =
+        _text
+}
